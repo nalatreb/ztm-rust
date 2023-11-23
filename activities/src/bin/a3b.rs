2ee61467c3aef64878a091e0cfa3b55bdf4b8c50
bertalanV <verebelyi.bertalan@zengo.eu>
2023-11-24 00:08:58 +0100

    initial commit

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -9,4 +9,14 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let my_variable = 23;
+
+    if my_variable > 5 {
+        println!(">5");
+    } else if my_variable < 5 {
+        println!("<5");
+    } else {
+        println!("=5");
+    }
+}
